bde829c626d9e2a1bbdd4a1acc4a57c0f730abd1
so0126 <nada981226@gmail.com>
2025-11-21 18:59:15 +0900

    refactor: imageSearchPlacesId -> s뺌

--- src/main/java/com/example/demo/dto/imageSearch/ImageSearchResult.java
+++ src/main/java/com/example/demo/dto/imageSearch/ImageSearchResult.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ImageSearchResult {
     private Long id;
-    private Long imageSearchPlacesId;
+    private Long imageSearchPlaceId;
     private Long placeId;
     private Boolean isSelected;
     private Long rank;
