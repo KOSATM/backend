f99de5e5ab27e2f21d34882f82ca38b2fe068c6c
dlgudwn55 <dkrlekeheoek@naver.com>
2025-11-24 11:31:43 +0900

    chore: 생성자주입->필드주입

--- src/main/java/com/example/demo/planner/controller/PlannerController.java
+++ src/main/java/com/example/demo/planner/controller/PlannerController.java
@@ -2,19 +2,33 @@ package com.example.demo.planner.controller;
 
 import com.example.demo.dto.travel.TravelPlan;
 import com.example.demo.planner.service.PlannerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/planner")
-@RequiredArgsConstructor
+@Slf4j
 public class PlannerController {
-
-    private final PlannerService plannerService;
+    @Autowired
+    private PlannerService plannerService;
 
     @PostMapping("/plans")
     public String createPlan(@RequestBody TravelPlan travelPlan) {
         plannerService.createTravelPlan(travelPlan);
         return "Travel Plan Created! ID: " + travelPlan.getId();
+    }
+
+    @PostMapping(
+    value = "/chat",
+    consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+    produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String chat(@RequestParam("question") String question) {
+        String answer = plannerService.chat(question);
+        return answer;
     }
 }
