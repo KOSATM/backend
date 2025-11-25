6892c904a1032e34b9462741cdefac2542312190
so0126 <nada981226@gmail.com>
2025-11-25 14:59:35 +0900

    feat: s3 Service metadata 반영 코드 추가

--- src/main/java/com/example/demo/common/s3/service/S3Service.java
+++ src/main/java/com/example/demo/common/s3/service/S3Service.java
@@ -1,12 +1,15 @@
 package com.example.demo.common.s3.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -17,16 +20,30 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String uploadFileToS3(MultipartFile file, String storedName) {
-        try {
-            amazonS3.putObject(
-                new PutObjectRequest(bucket, storedName, file.getInputStream(), null)
-                    .withCannedAcl(CannedAccessControlList.PublicRead)
-            );
-        } catch (Exception e) {
-            throw new RuntimeException("S3 업로드 실패", e);
-        }
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
-        return "https://" + bucket + ".s3.amazonaws.com/" + storedName;
+    public String uploadFile(MultipartFile file, String storedName) {
+
+        try {
+            // 메타데이터
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+
+            // S3 업로드
+            amazonS3.putObject(bucket, storedName, file.getInputStream(), metadata);
+
+            // 업로드 후 접근 URL 반환
+            return String.format(
+                "https://%s.s3.%s.amazonaws.com/%s",
+                bucket,
+                region,
+                storedName
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException("S3 upload failed", e);
+        }
     }
 }
