ab5e2aac104125e15541405b5e97791395f8b93f
so0126 <nada981226@gmail.com>
2025-11-25 14:26:32 +0900

    rename: Review 붙여서 이름 재작성

--- src/main/java/com/example/demo/travelgram/review/dto/request/ReviewPhotoUploadRequest.java
+++ src/main/java/com/example/demo/travelgram/review/dto/request/ReviewPhotoUploadRequest.java
@@ -3,8 +3,7 @@ package com.example.demo.travelgram.review.dto.request;
 import lombok.Data;
 
 @Data
-public class PhotoUploadRequest {
+public class ReviewPhotoUploadRequest {
     private Long groupId;
-    private String fileName;
     private Integer orderIndex;
 }
--- src/main/java/com/example/demo/travelgram/review/dto/response/ReviewPhotoUploadResponse.java
+++ src/main/java/com/example/demo/travelgram/review/dto/response/ReviewPhotoUploadResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class PhotoUploadResponse {
+public class ReviewPhotoUploadResponse {
     private Long photoId;
     private String fileUrl;
 }
