8c64bf0e6536f206e7a20f31c92ccfda7dbc376a
cake <cakesniffer3237@gmail.com>
2025-11-25 11:19:39 +0900

    서포터 폴더링

--- src/main/java/com/example/demo/supporter/imageSearch/dto/entity/ImageSearchPlace.java
+++ src/main/java/com/example/demo/supporter/imageSearch/dto/entity/ImageSearchPlace.java
@@ -1,12 +1,13 @@
-package com.example.demo.supporter.checklist.dto;
+package com.example.demo.supporter.imageSearch.dto.entity;
+
 
 import java.time.OffsetDateTime;
 import lombok.Data;
 
 @Data
-public class Checklist {
+public class ImageSearchPlace {
     private Long id;
     private Long userId;
-    private Integer dayIndex;
     private OffsetDateTime createdAt;
+    private String actionType;
 }
