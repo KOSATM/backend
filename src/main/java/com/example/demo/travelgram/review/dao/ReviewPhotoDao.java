2b8f439403fc3e8fe1544e3d1e14cb3fda7d5d93
so0126 <nada981226@gmail.com>
2025-11-25 21:42:28 +0900

    refactor: photoDato 안에 photoGroup도 같이 처리하도록 수정

--- src/main/java/com/example/demo/travelgram/review/dao/ReviewPhotoDao.java
+++ src/main/java/com/example/demo/travelgram/review/dao/ReviewPhotoDao.java
@@ -3,14 +3,13 @@ package com.example.demo.travelgram.review.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.travelgram.review.dto.entity.ReviewPhoto;
+import com.example.demo.travelgram.review.dto.entity.ReviewPhotoGroup;
 
 
 @Mapper
 public interface ReviewPhotoDao {
 
-    void insert(ReviewPhoto photo);
-    
-    // int insertPhotoGroup(ReviewPhotoGroup group);
-    // int insertPhoto(ReviewPhoto photo);
+    void insertReviewPhotoGroup(ReviewPhotoGroup group);
+    void insertReviewPhoto(ReviewPhoto photo);
     // List<ReviewPhoto> findPhotosByGroup(Long groupId);
 }
