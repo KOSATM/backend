b2a40c6b3237534400678fbf84ca8b59cf6668a2
dlgudwn55 <dkrlekeheoek@naver.com>
2025-11-26 09:13:26 +0900

    chore: 어노테이션 수정

--- src/main/java/com/example/demo/planner/travel/dto/entity/TravelPlanSnapshot.java
+++ src/main/java/com/example/demo/planner/travel/dto/entity/TravelPlanSnapshot.java
@@ -2,9 +2,9 @@ package com.example.demo.planner.travel.dto.entity;
 
 import java.time.OffsetDateTime;
 
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 public class TravelPlanSnapshot {
     private Long id;
     private Long userId;
