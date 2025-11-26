fb8cfba60d75d121d7a58ee3d32cc17a2515b6ca
so0126 <nada981226@gmail.com>
2025-11-26 10:31:18 +0900

    remove: column remove 반영

--- src/main/java/com/example/demo/travelgram/review/dto/entity/ReviewPhoto.java
+++ src/main/java/com/example/demo/travelgram/review/dto/entity/ReviewPhoto.java
@@ -11,13 +11,7 @@ public class ReviewPhoto {
     private Long id;
     private Long groupId;
     private String fileUrl;
-    // private String originalName;
-    // private String storedName;
     private Integer orderIndex;
 
-    // private double lat;
-    // private double lng;
-    
-    // private OffsetDateTime takenAt; // TZ 명시 필요, 서버에서 강제 변환 로직이용, KST로 변경해서 사용
     private OffsetDateTime createdAt;
 }
