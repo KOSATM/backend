043049db63698541345df6118f920e97b6494cd9
so0126 <nada981226@gmail.com>
2025-11-27 16:51:52 +0900

    feat: AiReviewDao에 insert문 추가, 하나의 aggregate

--- src/main/java/com/example/demo/travelgram/aiReview/dao/AiReviewDao.java
+++ src/main/java/com/example/demo/travelgram/aiReview/dao/AiReviewDao.java
@@ -1,5 +1,9 @@
 package com.example.demo.travelgram.aiReview.dao;
 
+import com.example.demo.travelgram.aiReview.dto.entity.*;
 public interface AiReviewDao {
+    void insertAiReview(AiReviewAnalysis aiReviewAnalysis);
+    void insertAiReviewHashtag(AiReviewHashtag aiReviewHashtag);
+    void insertAiReviewStyle(AiReviewStyle aiReviewStyle);
     
 }
