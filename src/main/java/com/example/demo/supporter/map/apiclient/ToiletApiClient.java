7f9004511ba2f451ab7adeeded4c10903547bc89
so0126 <nada981226@gmail.com>
2025-11-27 14:42:37 +0900

    fix: ApiClient 임포트 잘못된거 고침

--- src/main/java/com/example/demo/supporter/map/apiclient/ToiletApiClient.java
+++ src/main/java/com/example/demo/supporter/map/apiclient/ToiletApiClient.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.List;
 
 import org.springframework.stereotype.Component;
 
