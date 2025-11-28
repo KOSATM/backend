57b03d8e668b46a6fa60c038b339761df2115531
dlgudwn55 <dkrlekeheoek@naver.com>
2025-11-28 18:20:29 +0900

    refactor: TravelDao를 TravelPlanDao, TravelDayDao, TravelPlaceDao로 분리

--- src/main/java/com/example/demo/planner/travel/dao/TravelDao.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.planner.travel.dao;
-
-public class TravelDao {
-
-}
--- src/main/java/com/example/demo/planner/travel/service/TravelService.java
+++ src/main/java/com/example/demo/planner/travel/service/TravelService.java
@@ -1,5 +1,18 @@
 package com.example.demo.planner.travel.service;
 
-public class TravelService {
+import com.example.demo.planner.travel.dao.TravelDayDao;
+import com.example.demo.planner.travel.dao.TravelPlaceDao;
+import com.example.demo.planner.travel.dao.TravelPlanDao;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TravelService {
+  private final TravelPlanDao travelPlanDao;
+  private final TravelDayDao travelDayDao;
+  private final TravelPlaceDao travelPlaceDao;
+
+  
 }
