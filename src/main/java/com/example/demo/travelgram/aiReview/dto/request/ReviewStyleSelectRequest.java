d7246b465d42e2e289f3cad5f6553438e90efc2d
so0126 <nada981226@gmail.com>
2025-11-29 10:03:49 +0900

    feat: db에 맞게 수정

--- src/main/java/com/example/demo/travelgram/aiReview/dto/request/ReviewStyleSelectRequest.java
+++ src/main/java/com/example/demo/travelgram/aiReview/dto/request/ReviewStyleSelectRequest.java
@@ -1,5 +1,10 @@
 package com.example.demo.travelgram.aiReview.dto.request;
 
+import lombok.Data;
+
+@Data
 public class ReviewStyleSelectRequest {
-    
+    private Long styleId;
+    private Long analysisId;
+    private Long postId;
 }
