8e72c48799ea8e26ae5940587b24fdc381573e0a
dlgudwn55 <dkrlekeheoek@naver.com>
2025-11-29 14:00:15 +0900

    feat: DB 조회 에이전트 작성

--- src/main/java/com/example/demo/planner/travel/service/TravelService.java
+++ src/main/java/com/example/demo/planner/travel/service/TravelService.java
@@ -1,8 +1,10 @@
 package com.example.demo.planner.travel.service;
 
+import com.example.demo.planner.travel.agent.InternetSearchAgent;
 import com.example.demo.planner.travel.dao.TravelDayDao;
 import com.example.demo.planner.travel.dao.TravelPlaceDao;
 import com.example.demo.planner.travel.dao.TravelPlanDao;
+import com.example.demo.planner.travel.dao.TravelPlanSnapshotDao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,8 @@ public class TravelService {
   private final TravelPlanDao travelPlanDao;
   private final TravelDayDao travelDayDao;
   private final TravelPlaceDao travelPlaceDao;
+  private final TravelPlanSnapshotDao travelPlanSnapshotDao;
 
-  
+  private final InternetSearchAgent internetSearchAgent;
+
 }
