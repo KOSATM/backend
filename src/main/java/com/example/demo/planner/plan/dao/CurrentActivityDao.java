5c8b0b3411c6d2be382e0295eaee07b27d3d8804
so0126 <nada981226@gmail.com>
2025-12-01 17:05:55 +0900

    feat: currentActivityDao 추가

--- src/main/java/com/example/demo/planner/plan/dao/CurrentActivityDao.java
+++ src/main/java/com/example/demo/planner/plan/dao/CurrentActivityDao.java
@@ -2,31 +2,15 @@ package com.example.demo.planner.plan.dao;
 
 import java.util.List;
 
-import com.example.demo.planner.plan.dto.entity.Plan;
+import com.example.demo.planner.plan.dto.entity.CurrentActivity;
 
-public class CurrentActivityDao {
-        /**
-     * ID로 여행 계획을 조회합니다.
-     */
-    Plan selectTravelPlanById(Long id);
+public interface CurrentActivityDao {
+
+    void insertCurrentActivity(CurrentActivity currentActivity);
+
+    CurrentActivity selectCurrentActivityById(Long planPlaceId);
+
+    List<CurrentActivity> selectCurrentActivitiesByPlanId(Long planPlaceId);
     
-    /**
-     * 사용자의 모든 여행 계획을 조회합니다.
-     */
-    List<Plan> selectTravelPlansByUserId(Long userId);
-    
-    /**
-     * 여행 계획을 생성합니다.
-     */
-    int insertTravelPlan(Plan travelPlan);
-    
-    /**
-     * 여행 계획을 수정합니다.
-     */
-    int updateTravelPlan(Plan travelPlan);
-    
-    /**
-     * 여행 계획을 삭제합니다.
-     */
-    int deleteTravelPlan(Long id);
+    int deleteCurrentActivity(Long id);
 }
