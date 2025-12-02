c34019637ddfbeb26c6e2e8de55b9b2df0cd67c3
so0126 <nada981226@gmail.com>
2025-12-02 17:15:42 +0900

    feat: update title 집어넣기

--- src/main/java/com/example/demo/planner/plan/dao/PlanDao.java
+++ src/main/java/com/example/demo/planner/plan/dao/PlanDao.java
@@ -1,10 +1,11 @@
 package com.example.demo.planner.plan.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.planner.plan.dto.entity.Plan;
-
-import java.util.List;
 
 @Mapper
 public interface PlanDao {
@@ -28,6 +29,8 @@ public interface PlanDao {
      * 여행 계획을 수정합니다.
      */
     int updatePlan(Plan Plan);
+
+    void updatePlanTitleById(@Param("id") Long id, @Param("title") String title);
     
     /**
      * 여행 계획을 삭제합니다.
