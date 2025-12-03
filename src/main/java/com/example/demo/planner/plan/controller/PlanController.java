7c197b11b8d1353523935f417a80ce875a8fa4a6
dlgudwn55 <dkrlekeheoek@naver.com>
2025-12-03 14:30:48 +0900

    feat: snapshot parser 구현

--- src/main/java/com/example/demo/planner/plan/controller/PlanController.java
+++ src/main/java/com/example/demo/planner/plan/controller/PlanController.java
@@ -1,5 +1,29 @@
 package com.example.demo.planner.plan.controller;
 
-public class PlanController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.planner.plan.service.PlanService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class PlanController {
+  private final PlanService planService;
+
+  @GetMapping("/snapshot/search")
+  public String snapshotSearch(@RequestBody String snapshot) {
+    try {
+      planService.parseSnapshot(snapshot);
+      return "콘솔에 로그 있어요";
+    } catch (Exception e) {
+      return "에러";
+    }
+  }
+  
 }
--- src/main/java/com/example/demo/planner/plan/service/PlanService.java
+++ src/main/java/com/example/demo/planner/plan/service/PlanService.java
@@ -1,14 +1,22 @@
 package com.example.demo.planner.plan.service;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.planner.plan.agent.InternetSearchAgent;
 import com.example.demo.planner.plan.dao.PlanDao;
 import com.example.demo.planner.plan.dao.PlanDayDao;
 import com.example.demo.planner.plan.dao.PlanPlaceDao;
 import com.example.demo.planner.plan.dao.PlanSnapshotDao;
+import com.example.demo.planner.plan.dto.entity.PlanSnapshot;
+import com.example.demo.planner.plan.dto.response.PlanSnapshotContent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class PlanService {
@@ -16,7 +24,12 @@ public class PlanService {
   private final PlanDayDao planDayDao;
   private final PlanPlaceDao planPlaceDao;
   private final PlanSnapshotDao planSnapshotDao;
-
-  private final InternetSearchAgent internetSearchAgent;
+  
+  // 스냅샷을 여행 계획, 여행 일자, 여행 장소로 분리
+  public PlanSnapshotContent parseSnapshot(String snapshotJson) throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+    PlanSnapshotContent planSnapshotContent = objectMapper.readValue(snapshotJson, PlanSnapshotContent.class);
+    return planSnapshotContent;
+  }
 
 }
