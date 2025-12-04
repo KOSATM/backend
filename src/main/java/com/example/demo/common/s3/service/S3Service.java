1f0ea5113cee51d6956bb78ed52910ad5b2dd685
so0126 <nada981226@gmail.com>
2025-12-04 23:45:48 +0900

    feat: S3 Service에 delete 추가 및 url return sdk에 생성 맡김

--- src/main/java/com/example/demo/common/s3/service/S3Service.java
+++ src/main/java/com/example/demo/common/s3/service/S3Service.java
@@ -10,7 +10,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3Service {
@@ -34,16 +36,26 @@ public class S3Service {
             // S3 업로드
             amazonS3.putObject(bucket, storedName, file.getInputStream(), metadata);
 
-            // 업로드 후 접근 URL 반환
-            return String.format(
-                "https://%s.s3.%s.amazonaws.com/%s",
-                bucket,
-                region,
-                storedName
-            );
+            // ✅ [추천] URL 생성을 SDK에게 맡기기 (더 안전함)
+            return amazonS3.getUrl(bucket, storedName).toString();
 
         } catch (IOException e) {
             throw new RuntimeException("S3 upload failed", e);
         }
     }
+
+    public void deleteFile(String fileUrl) {
+    try {
+        // fileUrl에서 key(storedName)만 추출하는 로직 필요
+        // 예: https://bucket.s3.region.amazonaws.com/reviewPhotos/abc.jpg 
+        // -> reviewPhotos/abc.jpg 추출
+        String splitStr = ".com/";
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf(splitStr) + splitStr.length());
+
+        amazonS3.deleteObject(bucket, fileName);
+    } catch (Exception e) {
+        // 삭제 실패는 로그만 남기고 넘어가는 경우가 많음 (시스템 장애로 번지지 않게)
+        log.error("Error deleting file from S3: " + fileUrl, e);
+    }
+}
 }
