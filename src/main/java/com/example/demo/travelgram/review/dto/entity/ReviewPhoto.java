26dae3ed4f757bfe4d590aa8bf3af9256de1bf27
so0126 <nada981226@gmail.com>
2025-12-06 10:30:47 +0900

    fix: xml, dto가 순서 맞지 않아도 되도록 @NoArgs, @AllArgs 추가

--- src/main/java/com/example/demo/travelgram/review/dto/entity/ReviewPhoto.java
+++ src/main/java/com/example/demo/travelgram/review/dto/entity/ReviewPhoto.java
@@ -2,9 +2,13 @@ package com.example.demo.travelgram.review.dto.entity;
 
 import java.time.OffsetDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor  // ✅ MyBatis가 "이름 기반"으로 매핑할 수 있게 빈 생성자 허용
+@AllArgsConstructor // ✅ @Builder는 전체 생성자가 필요하므로 세트로 추가
 @Builder
 @Getter
 public class ReviewPhoto {
