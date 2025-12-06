ff3246c80890f88788bba1e4195e812cd59d18f1
so0126 <nada981226@gmail.com>
2025-12-06 12:30:57 +0900

    feat: Style 생성시 저장할 dto 추가

--- src/main/java/com/example/demo/travelgram/review/ai/dto/response/AiReviewStyleResponse.java
+++ src/main/java/com/example/demo/travelgram/review/ai/dto/response/AiReviewStyleResponse.java
@@ -2,8 +2,8 @@ package com.example.demo.travelgram.review.ai.dto.response;
 
 import java.util.List;
 
+import com.example.demo.travelgram.review.ai.dto.entity.AiReviewHashtag;
 import com.example.demo.travelgram.review.ai.dto.entity.AiReviewStyle;
-import com.example.demo.travelgram.review.dto.entity.ReviewHashtag;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,5 +12,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class AiReviewStyleResponse {
     private AiReviewStyle style; //caption 포함 되어있음
-    private List<ReviewHashtag> hashtags;
+    private List<AiReviewHashtag> hashtags;
 }
