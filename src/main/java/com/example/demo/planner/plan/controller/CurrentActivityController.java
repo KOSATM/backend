97a8accaa36690b8dd69fbcf3a6dc1dc7f495518
cake <cakesniffer3237@gmail.com>
2025-12-15 10:27:35 +0900

    feat: caapi 업서트랑 조회 추가

--- src/main/java/com/example/demo/planner/plan/controller/CurrentActivityController.java
+++ src/main/java/com/example/demo/planner/plan/controller/CurrentActivityController.java
@@ -2,6 +2,8 @@ package com.example.demo.planner.plan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,20 @@ public class CurrentActivityController {
 
     @Autowired
     private CurrentActivityService service;
+
+    // planPlaceId로 current activity 조회
+    @GetMapping("/{planPlaceId}")
+    public ResponseEntity<CurrentActivity> getActivityByPlanPlaceId(@PathVariable("planPlaceId") Long planPlaceId) {
+        log.info("조회 요청: planPlaceId={}", planPlaceId);
+        CurrentActivity activity = service.getActivityByPlanPlaceId(planPlaceId);
+        return ResponseEntity.ok(activity);
+    }
+
+    // current activity Upsert
     @PostMapping
     public ResponseEntity<String> saveCurrentActivity(@RequestBody CurrentActivity currentActivity) {
         log.info("받은 데이터: {}", currentActivity);
-        service.saveCurrentActivity(currentActivity);
+        service.saveOrUpdateCurrentActivity(currentActivity);
         return ResponseEntity.ok("Current activity saved successfully");
     }
 }
